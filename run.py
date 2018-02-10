ae149cc066adfa16f013c2d9d50be07ed04fcbfe
Ariel Uy <auy@andrew.cmu.edu>
2018-02-10 13:40:02 -0500

    Add existing file

--- run.py
+++ run.py
@@ -1,12 +1,2 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-
-
-@app.route("/")
-def template_test():
-    return render_template('exampletemp.html', my_string="Wheeeee!", my_list=[0,1,2,3,4,5])
-
-template_test()
-
-if __name__ == '__main__':
-    app.run(debug=True)
+from jinja2 import Environment, PackageLoader, select_autoescape
+evn = Environment(loader=PackageLoader('application','templates'),autoescape=select_autoescape(['html','xml'])))
