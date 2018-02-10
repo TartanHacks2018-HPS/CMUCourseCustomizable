d49497ea25da6f21d35180155876126ffbf2fe1c
atapritchard <atapritchard@gmail.com>
2018-02-10 14:26:55 -0500

    Add existing file

--- Converter.py
+++ Converter.py
@@ -6,18 +6,17 @@ from cmu_course_api import *
 def convert():
     inputfile = open('master_data.txt','r')
     master = ast.literal_eval(inputfile.readline())
-    mlist = []
+    dictionary = {}
     keys = master.keys()
-    counter = 0
     for key in keys:
-        mlist.append([key])
+        mlist = []
         course = master.get(key)
         coursekeys = course.keys()
         for item in coursekeys:
-            mlist[counter].append(course[item])
-        counter += 1
+            mlist.append(course[item])
+        dictionary[key] = mlist
     newfile = open('converted.txt','w')
-    newfile.write(str(mlist))
+    newfile.write(str(dictionary))
     return None
 
 convert()
