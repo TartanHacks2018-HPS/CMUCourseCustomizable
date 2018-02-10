c21fbe2c619a9f7a9c71e85c37bccc5f4c6d1733
Dahoas <Dahoas@LAPTOP-T31M51R7.localdomain>
2018-02-10 11:12:17 -0500

    Add existing file

--- PostRec.py
+++ PostRec.py
@@ -1,12 +1,20 @@
 import sys
 import os
 from cmu_course_api import *
+import ast
 
 def postReq_calc(master_file):
     inputfile = open(master_file,"r")
     master = ast.literal_eval(inputfile.readline())
     keys = master.keys()
     for key in keys:
+        print(key)
         prereqs = master.get(key).get("prereqs")
-        for entry in prereqs:
-            master.get(entry).get("postreqs").append(key)
+        print(prereqs)
+        if(prereqs != None):
+            for entry in prereqs:
+                print (entry)
+                print (master.get(entry).get("postreqs"))
+                master.get(entry).get("postreqs").append(key)
+
+postReq_calc("master_data.txt")
