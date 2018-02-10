ab025474df13b5987026026229b229d70818ecf1
atapritchard <atapritchard@gmail.com>
2018-02-09 21:43:44 -0500

    Add existing file

--- Demo.py
+++ Demo.py
@@ -2,4 +2,12 @@ import sys
 import os
 from cmu_course_api import *
 
-print(get_course_data('F'))
+
+#Saves data to given filename
+def save_data(newfile,text):
+    inputfile = open(os.path.join(data_folder,newfile),'w')
+    inputfile.write(text)
+    inputfile.close()
+    return None
+
+#fall = get_course_data('F')
