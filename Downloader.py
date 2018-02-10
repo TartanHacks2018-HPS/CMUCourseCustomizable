0dcd4678a38425618e97c578bb6738aea2ec9136
atapritchard <atapritchard@gmail.com>
2018-02-10 01:54:22 -0500

    Add existing file

--- Downloader.py
+++ Downloader.py
@@ -2,6 +2,26 @@ import sys
 import os
 from cmu_course_api import *
 
+def findfirst(lst, key):
+    for idx in range(0,len(lst)):
+        if lst[idx] == key:
+            return idx
+    return -1
+
+def clean(reqs):
+    if reqs != None:
+        for idx in range(0,len(reqs)):
+            char = reqs[idx:idx+1]
+            if char == ' ' or char == 'a' or char == 'n' or char == 'o':
+                char = ''
+                reqs = reqs[0:idx] + char + reqs[idx+1:]
+                idx -= 1
+            else:
+                if char == 'd' or char == 'r':
+                    char = ','
+                reqs = reqs[0:idx] + char + reqs[idx+1:]
+    return reqs
+
 def get_data():
     newfile = open('master_data.txt','w')
     fall = get_course_data('F').get('courses')
@@ -14,6 +34,8 @@ def get_data():
         keys = item.keys()
         for key in keys:
             course = item.get(key)
+            course['prereqs'] = clean(course.get('prereqs'))
+            course['coreqs'] = clean(course.get('coreqs'))
             course.pop('prereqs_obj')
             course.pop('coreqs_obj')
             course.pop('lectures')
