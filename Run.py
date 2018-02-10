2adbc3736780e2b636bc1b7a623d1dd5e1fdba28
Dahoas <Dahoas@LAPTOP-T31M51R7.localdomain>
2018-02-10 12:38:52 -0500

    Add existing file

--- Run.py
+++ Run.py
@@ -1,20 +1,2 @@
-from jinja2 import Template
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-
-@app.route("/")
-def template_test():
-    return render_template('GUI.html', my_list=[0,1,2,3,4,5])
-
-
-if __name__ == 'GUI.html':
-    #app.run(debug=True)
-
-    t = Template("Hello {{ something }}!")
-    t.render(something="World")
-    t = Template("My favorite numbers: {% for n in range(1,10) %}{{n}} " "{% endfor %}")
-    t.render()
-
-template_test()
+from jinja2 import Environment, PackageLoader, select_autoescape
+evn = Environment(loader=PackageLoader('application','templates'),autoescape=select_autoescape(['html','xml'])))
